6c653d9aed01c64c5168763fa21ee80584ffe078
Manu Moraira Garcia <137888375+manummg10@users.noreply.github.com>
2025-08-12 12:54:12 +0200

    Add files via upload

---
+++ Tarjeta Digital/assets/js/particles-config.js
@@ -0,0 +1,14 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 60 },
+    size: { value: 3 },
+    color: { value: "#ffffff" },
+    line_linked: { enable: true, color: "#ffffff" },
+    move: { speed: 2 }
+  },
+  interactivity: {
+    events: { onhover: { enable: true, mode: "repulse" } }
+  }
+});
+
+
